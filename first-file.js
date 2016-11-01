b8d5045fe5e9b93d885530331c443b1420069cc8
dramirez <ramirezdanilu6@gmail.com>
2016-11-01 17:52:30 +0000

    skills based javascript.

---
+++ first-file.js
@@ -0,0 +1,23 @@
+// this is my comment yo
+var saying = "yeaaaaa";
+console.log(saying); 
+
+// functions are my jam    
+var myFunction = function(number){
+  number = number + 5; 
+  console.log(number); 
+}; 
+var yellMyName =function (firstName, lastName){
+  console.log("Hey! Whats up", firstName.toUpperCase())
+  console.log(lastName);
+};
+
+yellMyName("Danilu", "Ramirez");
+
+var multiplyTwoNumbers = function (number1, number2){
+  console.log(number1 * number2)
+}; 
+
+multiplyTwoNumbers (10,5); // should equal 20 
+
+var 
